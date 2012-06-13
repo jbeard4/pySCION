0b09590e8e6561f13e72a874d2ce60c4ed304fb2
Jacob Beard <jbeard4@cs.mcgill.ca>
2012-06-13 11:03:29 -0400

    Basic example now works.

--- SCXML.py
+++ SCXML.py
@@ -1,6 +1,7 @@
 import spidermonkey
-from xml.dom.minidom import parse, parseString
+from lxml import etree
 import urllib2
+import pdb
 
 #create a new context
 rt = spidermonkey.Runtime()
@@ -12,6 +13,12 @@ es5jsStr = es5js.read()
 cx.execute(es5jsStr) 
 es5js.close() 
 
+#same for json2.js
+json2js = open("js-lib/json/json2.js")
+json2Str = json2js.read() 
+cx.execute(json2Str) 
+es5js.close() 
+
 #load up SCION
 scionjs = open("scion.js")
 scionJsStr = scionjs.read()
@@ -21,16 +28,14 @@ scionjs.close()
 #expose the js API here
 modelFactory = cx.execute("""
     (function(){
-        JsonML = require('external/jsonml/jsonml-dom'),
-        annotator = require('core/util/annotate-scxml-json'),
-        json2model = require('core/scxml/json2model');
+        var annotator = require('core/util/annotate-scxml-json'),
+            json2model = require('core/scxml/json2model');
  
-        return function(doc){
+        return function(scxmlJsonString){
             //do the steps here to construct a model from a DOM object
             //should look pretty similar to the other adapters (node, rhino, browser)
 
-            var arr = JsonML.parseDOM(doc);
-            var scxmlJson = arr[1];
+            var scxmlJson = JSON.parse(scxmlJsonString);
 
             var annotatedScxmlJson = annotator.transform(scxmlJson);
 
@@ -51,22 +56,22 @@ interpreterFactory = cx.execute("""
     })();
 """)
 
+jsonMLTransform = etree.XSLT(etree.parse(open("lib/jsonml/jsonml.xslt")))
+
 def urlToModel(url):
     s = urllib2.urlopen(url).read()
     return documentStringToModel(s)
 
 def pathToModel(path):
-    f = open(path)
-    dom = parse(f)
-    f.close()
-    return documentToModel(dom)
+    return documentToModel(etree.parse(open(path)))
 
 def documentStringToModel(scxmlDocString):
-    dom = parseString(scxmlDocString)
-    return documentToModel(dom)
+    return documentToModel(etree.parse(StringIO(scxmlDocString)))
 
-def documentToModel(dom):
-    return modelFactory(dom)
+def documentToModel(scxmlDoc):
+    result = jsonMLTransform(scxmlDoc)
+    #pdb.set_trace()
+    return modelFactory(str(result))
 
 class SCXML:
 
@@ -74,8 +79,8 @@ class SCXML:
         #construct
         self.interpreter = interpreterFactory(model)  
 
-    def start():
+    def start(self):
         return self.interpreter.start()
 
-    def gen(event,data):
+    def gen(self,event,data={}):
         return self.interpreter.gen(event,data)
--- test.py
+++ test.py
@@ -3,3 +3,11 @@ from SCXML import pathToModel,SCXML
 m = pathToModel("test/basic1.scxml")
 print m
 
+interpreter = SCXML(m)
+print interpreter 
+
+initialConf = interpreter.start()
+print initialConf 
+
+nextConf = interpreter.gen("t")
+print nextConf 
